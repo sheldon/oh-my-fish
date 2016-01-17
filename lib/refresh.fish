557a789edda1d93b00c77e2559b158f05187cac4
Derek Stavis <dekestavis@gmail.com>
2016-01-17 21:00:35 -0200

    refresh: Fix #47. Also silence greeting.

--- lib/refresh.fish
+++ lib/refresh.fish
@@ -7,6 +7,10 @@
 function refresh -d "Refresh fish session by replacing current process"
   if not set -q CI
     history --save
+    set -gx dirprev $dirprev
+    set -gx dirnext $dirnext
+    set -gx dirstack $dirstack
+    set -gx fish_greeting ''
     exec fish < /dev/tty
   end
 end
